2904bccb2c634782a7de0c710a2ca79c5668ffef
kkuro5001 <kkuro5001@gmail.com>
2026-05-15 12:15:49 +0900

    YOLO追加

--- Python/main.py
+++ Python/main.py
@@ -3,6 +3,9 @@ import uvicorn
 
 app = FastAPI()
 
+#yoloのモデル読み込み
+model = YOLO("yolov8m.pt")
+
 # 画像を受け取って服のカテゴリや色を予測する
 @app.post("/predict")
 async def predict(request: Request):
@@ -10,9 +13,31 @@ async def predict(request: Request):
 
     print("画像受信完了")
 
-    #仮AIの処理をここに入れる YOLOなど
+    #YOLOでカテゴリを判別する
+    np_arr = np.frombuffer(image_bytes, np.uint8)
+    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+    #yoloで予測
+    results = model(img)
+
+    #予測結果からカテゴリと色を抽出する（ここでは仮の値を使用）
+    labels = results.names
+    boxes = results.boxes
+
+    if len(boxes) == 0:
+        return {
+            "category": "unknown",
+            "color": "unknown",
+            "suggestion": "no clothes detected"
+        }
+
+    #一番スコアが高いものを取得
+    cls_id = int(boxes.cls[0])
+    category = labels[cls_id]
+
+
     result = {
-        "category": "shirt",
+        "category": category,
         "color": "black",
         "suggestion": "white pants recommended"
     }
